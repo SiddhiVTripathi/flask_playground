f8af664c102c158ab4191b9b673927890ee2de3f
SiddhiVTripathi <siddhi19vinayak@gmail.com>
2020-08-02 22:05:46 +0530

    new file:   workspace/__pycache__/forms.cpython-38.pyc 	modified:   workspace/flaskBlog.py 	new file:   workspace/forms.py 	modified:   workspace/templates/layout.html 	new file:   workspace/templates/login.html 	new file:   workspace/templates/register.html

--- workspace/flaskBlog.py
+++ workspace/flaskBlog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'fcb4d56dc328bbb269db411972547c41'
 
 posts = [
     {
@@ -20,12 +23,27 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', posts=posts, title="Home")
 
 
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Craeted for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__=='__main__':

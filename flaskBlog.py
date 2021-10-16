1fc8f991a747bfa71bf13d6e97b2e9ccaa4783d7
SiddhiVTripathi <siddhi19vinayak@gmail.com>
2021-10-16 21:52:42 +0530

    Added registration form

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fcb4d56dc328bbb269db411972547c41'
--- forms.py
+++ forms.py
@@ -3,18 +3,10 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',validators=[DataRequired(), Length(min=2, max=20)])
-
+    name = StringField('Name',validators=[DataRequired(), Length(min=2, max=20)])
+    organization = StringField("Organization")
     email = StringField('Email',validators=[DataRequired(), Email()])
 
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    comment = StringField('Comment', validators=[DataRequired()])
 
-    submit = SubmitField('Sign up')
-
-class LoginForm(FlaskForm):
-    email = StringField('Email',validators=[DataRequired(), Email()])
-
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Sign in')
+    submit = SubmitField('Submit')
